61b05c38707b4c8c706c980aebc5eeef0ef99d80
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-12 23:01:18 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -5,5 +5,13 @@ def initialize(first_name, last_name)
   @first_name = first_name
   @last_name = last_name
 end
+
+def first_name(first_name)
+  @first_name
+end
+
+def last_name(last_name)
+  @last_name
+end
   
 end
